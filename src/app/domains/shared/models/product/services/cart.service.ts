cc363c6f9c26ad17ce2c33325b9f9d2f3493b99c
Daniel Suarez <0321daniels@gmail.com>
2025-02-09 14:25:40 -0500

    Se agregó la libreria pdfmake para descargar el carrito en pdf

--- src/app/domains/shared/models/product/services/cart.service.ts
+++ src/app/domains/shared/models/product/services/cart.service.ts
@@ -1,19 +1,27 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
 import { Product } from '../../Product';
 import { ProductComponent } from '../../../../vendedor/components/product/product.component';
 import { CreateItemDto } from '../../pedidos/dto/CreateItemDto';
 import Swal from 'sweetalert2';
 import { Producto } from '../entities/Producto';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  
+
   // cart = signal<Product[]>([]);
   cart = signal<Producto[]>([]);
   createItemDto = signal<CreateItemDto[]>([]);
   private shipment = 18000;
+  // imagenBase64: string = 'data:image/jpeg;base64,/';
+  imagenBase64: string = '';
+  // imagenBase64 = signal<string[]>([]);
+
+  baseUrl = environment.urlServices;
+  http = inject(HttpClient);
 
   subTotal = computed(() => {
     const cart = this.cart();
@@ -24,16 +32,19 @@ export class CartService {
     }, 0);
   });
 
-  
+
   total = computed(() => {
-    if(this.subTotal() != 0){
+    if (this.subTotal() != 0) {
       return this.subTotal() + this.shipment;
     }
     return 0;
   });
 
 
-  constructor() { }
+  constructor() {
+    this.getImagenBase64();
+
+  }
 
   addTocart(product: Producto | undefined, cantidad: number = 1){
     if (product) {
@@ -42,10 +53,30 @@ export class CartService {
     }
   }
 
-  cantidadEspecifica(idProducto: number | undefined){
+  // async addTocart(product: Producto | undefined, cantidad: number = 1) {
+  //   if (product) {
+  //     let convert = '';
+  //     if (product.imagenes && product.imagenes.length > 0) {
+  //       try {
+  //         const imageUrl = `${this.baseUrl}uploads/${product.imagenes[0]}`;
+  //         const base64Image = this.encodeToBase64(imageUrl);
+  //         convert = 'data:image/jpeg;base64,/' + base64Image; // Sobrescribe la URL con Base64
+  //       } catch (error) {
+  //         console.error('Error al convertir la imagen a Base64', error);
+  //       }
+  //     }
+
+  //     this.cart.update(prevState => [...prevState, product]);
+  //     this.createItemDto.update(prevState => [...prevState, { idProducto: product.idProducto, cantidad }]);
+  //     // this.imagenBase64.update(prevState => [...prevState,convert]);
+  //   }
+  // }
+
+
+  cantidadEspecifica(idProducto: number | undefined) {
     return this.createItemDto().find(item => item.idProducto === idProducto)?.cantidad;
   }
-  productExists(id: number | undefined){
+  productExists(id: number | undefined) {
     return this.cart().some(cartItem => cartItem.idProducto === id);
   }
 
@@ -55,8 +86,8 @@ export class CartService {
 
   incrementQuantity(idProducto: number | undefined) {
     if (idProducto !== undefined) {
-      this.createItemDto.update(prevState => 
-        prevState.map(item => 
+      this.createItemDto.update(prevState =>
+        prevState.map(item =>
           item.idProducto === idProducto ? { ...item, cantidad: (item.cantidad || 0) + 1 } : item
         )
       );
@@ -65,24 +96,59 @@ export class CartService {
 
   decrementQuantity(idProducto: number | undefined) {
     if (idProducto !== undefined) {
-      this.createItemDto.update(prevState => 
-        prevState.map(item => 
+      this.createItemDto.update(prevState =>
+        prevState.map(item =>
           item.idProducto === idProducto ? { ...item, cantidad: (item.cantidad || 0) - 1 } : item
         )
       );
     }
   }
 
-  getShipment(){
+  getShipment() {
     return this.shipment;
   }
 
-  setShipment(shipment: number){
+  setShipment(shipment: number) {
     this.shipment = shipment;
   }
 
-  clearCart(){
+  clearCart() {
     this.cart.update(() => []);
     this.createItemDto.update(() => []);
+  }
+
+  getImagenBase64() {
+    this.cart().forEach(async (product) => {
+      if (product.imagenes && product.imagenes.length > 0) {
+        // this.imagenBase64 = await this.imageUrlToBase64(this.baseUrl+'uploads/'+product.imagenes[0]);
+        // this.imagenBase64 += this.encodeToBase64(this.baseUrl+'uploads/'+ product.imagenes[0]);
+        this.imagenBase64 = this.baseUrl + 'uploads/' + product.imagenes[0];
+      }
+    });
+  }
+
+  encodeToBase64(text: string): string {
+    return btoa(text);
+  }
+
+  // Método para convertir desde una URL
+  async imageUrlToBase64(imageUrl: string): Promise<string> {
+    try {
+      const blob = await this.http.get(imageUrl, { responseType: 'blob' }).toPromise();
+      return await this.blobToBase64(blob!);
+    } catch (error) {
+      console.error('Error converting image:', error);
+      throw error;
+    }
+  }
+
+  // Conversión genérica desde Blob
+  private blobToBase64(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result as string);
+      reader.onerror = error => reject(error);
+      reader.readAsDataURL(blob);
+    });
   }
 }
--- src/app/domains/vendedor/pages/catalog/cart/cart.component.ts
+++ src/app/domains/vendedor/pages/catalog/cart/cart.component.ts
@@ -1,16 +1,17 @@
-import { Component, inject, signal, SimpleChanges, Input } from '@angular/core';
+import { Component, inject, signal, SimpleChanges, Input, PLATFORM_ID } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { Product } from '../../../../shared/models/Product';
 import { CartService } from '../../../../shared/models/product/services/cart.service';
 import { ProductService } from '../../../../shared/models/product/services/product.service';
 import { CommonModule } from '@angular/common';
-import { AppComponent } from '../../../../../app.component';
-import generatePDF from '../../../../shared/components/pdf';
+// import generatePDF from '../../../../shared/components/pdf';
+import generatePDF from '../../../../shared/components/pdfMake';
 import Swal from 'sweetalert2';
 import { Producto } from '../../../../shared/models/product/entities/Producto';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { MensajeService } from '../../../../shared/mensaje/mensaje.service';
 import { environment } from '../../../../../../environments/environment';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-cart',
@@ -19,39 +20,42 @@ import { environment } from '../../../../../../environments/environment';
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent{
+export class CartComponent {
 
-    products = signal<Product[]>([]);
-    private cartService = inject(CartService);
-    private productService = inject(ProductService);
-    private mensaje = inject(MensajeService);
+  products = signal<Product[]>([]);
+  private cartService = inject(CartService);
+  private productService = inject(ProductService);
+  private mensaje = inject(MensajeService);
+  private platformId = inject(PLATFORM_ID);
 
-    cart = this.cartService.cart; //signal<Product[]>([]);
-    total = this.cartService.total; //signal(0);
-    subTotal = this.cartService.subTotal; //signal(0);
-    shipment = this.cartService.getShipment();//10000
-    createItemDto = this.cartService.createItemDto;
+  cart = this.cartService.cart; //signal<Product[]>([]);
+  total = this.cartService.total; //signal(0);
+  subTotal = this.cartService.subTotal; //signal(0);
+  shipment = this.cartService.getShipment();//10000
+  createItemDto = this.cartService.createItemDto;
+  imagenBase64 = this.cartService.imagenBase64; //signal<string[]>([]);
 
-    baseUrl = environment.urlServices + 'uploads/';
+  baseUrl = environment.urlServices + 'uploads/';
 
-    isOpenModal = false;
-   
-    ngOnInit(){
-      
-    }
-    
-    ngOnChanges(changes: SimpleChanges){
-      this.getProducts(); 
-    }
-  
-    addToCart(product: Producto){
+  isOpenModal = false;
+
+  ngOnInit() {
+    this.cartService.getImagenBase64();
+    console.log('img', this.imagenBase64)
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.getProducts();
+  }
+
+  addToCart(product: Producto) {
     //  this.cart.update(prevState => [...prevState, product]);
     this.cartService.addTocart(product);
-    }
-  
-    private getProducts(){
-      // console.log(`category_id: ${category_id}`);
-      this.productService.getProducts()
+  }
+
+  private getProducts() {
+    // console.log(`category_id: ${category_id}`);
+    this.productService.getProducts()
       .subscribe({
         next: (products) => {
           this.products.set(products);
@@ -61,38 +65,47 @@ export class CartComponent{
           console.error(error);
         }
       });
-    }
-    removeItemProduct(productId: number | undefined) {
-      return this.cartService.removeItem(productId);
-    }
+  }
+  removeItemProduct(productId: number | undefined) {
+    return this.cartService.removeItem(productId);
+  }
 
-    cantidadEspecifica(idProducto: number | undefined){
-      return this.cartService.cantidadEspecifica(idProducto);
-    }
+  cantidadEspecifica(idProducto: number | undefined) {
+    return this.cartService.cantidadEspecifica(idProducto);
+  }
 
-    incrementQuantity(idProducto: number | undefined){
-      return this.cartService.incrementQuantity(idProducto);
-    }
+  incrementQuantity(idProducto: number | undefined) {
+    return this.cartService.incrementQuantity(idProducto);
+  }
 
-    decrementQuantity(idProducto: number | undefined){
-      const cantidad = this.cantidadEspecifica(idProducto);
-      if(cantidad && cantidad > 1){
-        return this.cartService.decrementQuantity(idProducto);
-      }else{
-        Swal.fire('Warning','La cantidad mínima es 1','warning');
-      }
-    }
-
-    clientValidate(){
-      if(this.cart().length === 0){
-        this.mensaje.showMessage('Información','No hay productos para proceder al pedido','warning');
-        return;
-      }
-      this.mensaje.showClientValidate();
-    }
-    
-    onGeneratePDF(){
-      const fecha = '2021-09-01';
-      // generatePDF(cart(),fecha);
+  decrementQuantity(idProducto: number | undefined) {
+    const cantidad = this.cantidadEspecifica(idProducto);
+    if (cantidad && cantidad > 1) {
+      return this.cartService.decrementQuantity(idProducto);
+    } else {
+      Swal.fire('Warning', 'La cantidad mínima es 1', 'warning');
     }
   }
+
+  clientValidate() {
+    if (this.cart().length === 0) {
+      this.mensaje.showMessage('Información', 'No hay productos para proceder al pedido', 'warning');
+      return;
+    }
+    this.mensaje.showClientValidate();
+  }
+
+  onGeneratePDF() {
+    if (isPlatformBrowser(this.platformId)) {
+      import('pdfmake/build/pdfmake').then(pdfMakeModule => {
+        import('pdfmake/build/vfs_fonts').then(pdfFontsModule => {
+          const pdfMake = pdfMakeModule.default;
+          pdfMake.vfs = pdfFontsModule.vfs;
+          // Aquí ya puedes usar pdfMake.createPdf(...)
+          const fecha = new Date().toLocaleDateString();
+          generatePDF(this.cart(), fecha, this.cartService);
+        });
+      });
+    }
+  }
+}
