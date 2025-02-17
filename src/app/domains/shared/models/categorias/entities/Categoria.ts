d486befb2f93f9c4ed069cc45afb11659e609c57
Andrés Esala <andresesala25@outlook.com>
2025-02-16 22:22:50 -0500

    fix: rename id_categoria to idCategoria for consistency in Categoria model

--- src/app/domains/shared/models/categorias/entities/Categoria.ts
+++ src/app/domains/shared/models/categorias/entities/Categoria.ts
@@ -1,7 +1,7 @@
 import { Producto } from "../../product/entities/Producto";
 
 export class Categoria {
-    id_categoria?: number;
+    idCategoria?: number;
     nombre?: string;
     productos?: Producto[];
 }
