272952389399145faf2cd5ec39818ec4e762bddc
Andrés Esala <andresesala25@outlook.com>
2025-03-02 23:02:11 -0500

    refactor: clean up CreateProductoDto by removing commented code

--- src/app/domains/shared/models/product/dto/CreateProductoDto.ts
+++ src/app/domains/shared/models/product/dto/CreateProductoDto.ts
@@ -5,10 +5,5 @@ export class CreateProductoDto {
     descripcion?: string;
     precio?: number;
     categorias?: number[];
-    insumos?: {idInsumo: number, cantidad: number}[];
+    insumos?: { idInsumo: number, cantidad: number}[];
 }
-
-// export interface InsumoProducto{
-//     idInsumo: string;
-//     cantidad: string;
-// }
