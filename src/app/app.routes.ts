fcf5e8a59b2e1d35ca87459ac392e73f5dd2d59f
Daniel Suarez <0321daniels@gmail.com>
2025-03-23 16:14:45 -0500

    refactor: remove unused Inventories component and clean up related files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './domains/shared/pages/login/login.component';
 import { LayoutComponent } from './domains/administrador/components/layout/layout.component';
-import { InventoriesComponent } from './domains/administrador/pages/inventories/inventories.component';
 import { ClientsComponent } from './domains/administrador/pages/clients/clients.component';
 
 import { CatalogComponent } from './domains/vendedor/pages/catalog/catalog.component';
